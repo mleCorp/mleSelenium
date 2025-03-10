303ea2b6272707ea5a25f35dd6124170043a2a1a
Miguel Louie J. Endoma Jr. <miguelendoma@gmail.com>
2025-03-10 05:40:46 +0000

    sample test issue 1 only

---
+++ src/test/java/com/mleselenium/SampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void sampleTestMethod() {
+        assertTrue(true, "This test should pass");
+    }
+}
